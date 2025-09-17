68ced5ea77433c6a61a9d43de1939060a80e2133
csjgoto <masanori.goto@csisolar.com>
2025-09-17 13:35:33 +0900

    sleep

--- src/classes/SMTPServer.ts
+++ src/classes/SMTPServer.ts
@@ -9,6 +9,7 @@ const Joiner = require('mailsplit').Joiner;
 import { Config } from './Config';
 import { prefixedLog } from './Logger';
 import { MailQueue } from './MailQueue';
+import crypto from "crypto";
 
 const log = prefixedLog('SMTPServer');
 
@@ -143,7 +144,12 @@ export class SMTPServer
         });
 
         // Create the EML file
-        const tmpFile = path.join(this.#queue.tempPath, `${session.id}.eml`);
+        //const tmpFile = path.join(this.#queue.tempPath, `${session.id}.eml`);
+        const hash = crypto
+            .createHash("sha256")
+            .update(stream + session.id)
+            .digest("hex");
+        const tmpFile = path.join(this.#queue.tempPath, `${hash}.eml`);
         const writeStream = fs.createWriteStream(tmpFile);
         const mailCompile = mail.compile();
         (mailCompile as any).keepBcc = true;
@@ -166,7 +172,6 @@ export class SMTPServer
                 });
             }
         });
-        setTimeout(() => {}, 500);
     };
     
 }
